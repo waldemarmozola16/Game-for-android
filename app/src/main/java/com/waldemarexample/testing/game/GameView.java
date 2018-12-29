8c3ed6aca7b84e6893af9066b4a65f6d9b57755b
Waldemar Mozola <waldemar.mozola@onet.pl>
2018-12-29 03:05:22 +0100

    override

---
+++ app/src/main/java/com/waldemarexample/testing/game/GameView.java
@@ -0,0 +1,23 @@
+package com.waldemarexample.testing.game;
+
+import android.content.Context;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+public abstract class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    public GameView(Context context){
+        super(context);
+
+        getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
+
+
+    }
+
+    @Override
+    public void
+}
